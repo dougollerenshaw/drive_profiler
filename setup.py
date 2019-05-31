f4dbf643a9511eddff72aeada1cb66030222a0fb
Doug Ollerenshaw <d.ollerenshaw@gmail.com>
2019-05-31 11:37:31 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="drive_profiler",
+    version="0.1",
+    author="Doug Ollerenshaw",
+    author_email="dougo@alleninstitute.org","d.ollerenshaw@gmail.com",
+    description="python drive profiler",
+    install_requires=[
+        "pandas",
+        "click"
+    ]
+)
